51aed58a4132237a1e80ceedd62fbe0050f281cf
Robin <robin.zhangx@gmail.com>
2018-06-21 11:51:13 +0800

    Add CoboSDK.framework

---
+++ CoboSDK.framework/Headers/CoboSDK.h
@@ -0,0 +1,18 @@
+//
+//  CoboSDK.h
+//  CoboSDK
+//
+//  Created by Robin Zhang on 2018/5/23.
+//  Copyright © 2018 Cobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoboSDK.
+FOUNDATION_EXPORT double CoboSDKVersionNumber;
+
+//! Project version string for CoboSDK.
+FOUNDATION_EXPORT const unsigned char CoboSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoboSDK/PublicHeader.h>
+
